729c9e4ec8b262636205c01cdfe53d571080c971
timjzee <timjzee@gmail.com>
2019-01-17 21:02:00 +0100

    Bugfix

--- makeChunkFile.py
+++ makeChunkFile.py
@@ -13,10 +13,10 @@ for line in f_lines:
     line_list = line.split(",")
     if counter == 1:
         with open(f_path + "chunks2.txt", "w") as f:
-            f.write("filepath," + ",".join(new_line_list[3:-1]))
+            f.write("filepath," + ",".join(line_list[3:-1]) + "\n")
         continue
     new_line = re.sub(r'"', "", line)
     new_line = new_line[5:]
     new_line_list = new_line.split(",")
     with open(f_path + "chunks2.txt", "a") as f:
-        f.write("/".join(new_line_list[:3]) + "," + ",".join(new_line_list[3:-1]))
+        f.write("/".join(new_line_list[:3]) + "," + ",".join(new_line_list[3:-1]) + "\n")
